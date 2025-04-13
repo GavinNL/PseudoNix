d5f2e200c19ce3e7b718c145dbebf451e4e18e84
Gavin <gavin.n.lobo@gmail.com>
2025-04-13 12:31:17 -0400

    Console widget bug

--- include/ebash/ImGuiConsoleWidget.h
+++ include/ebash/ImGuiConsoleWidget.h
@@ -92,7 +92,7 @@ struct ConsoleWindow
         // As a specific feature guaranteed by the library, after calling Begin() the last Item represent the title bar.
         // So e.g. IsItemHovered() will return true when hovering the title bar.
         // Here we create a context menu only available from the title bar.
-        if (ImGui::BeginPopupContextItem())
+        if (ImGui::BeginPopupContextItem("Console Popup"))
         {
             if (ImGui::MenuItem("Close Console"))
             {
